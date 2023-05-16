f5d838b0b16460b945759031f8b97cc821728e04
kodai3 <k3dai.su3@gmail.com>
2023-05-16 21:05:47 +0900

    fix: 🐛 add decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -15,3 +15,5 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = []
--- setupFiles.js
+++ setupFiles.js
@@ -2,7 +2,7 @@ import { decorators as nextjsDecorators } from '@storybook/nextjs/preview.js';
 import { setProjectAnnotations } from '@storybook/react';
 import * as globalStorybookConfig from './.storybook/preview';
 
-const decorators = [...nextjsDecorators];
+const decorators = [...nextjsDecorators, ...globalStorybookConfig.decorators];
 
 setProjectAnnotations({
   ...globalStorybookConfig,
