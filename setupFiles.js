a84f1ef5006743cb8cd10e24c286a16ce3a699e6
kodai3 <k3dai.su3@gmail.com>
2023-06-02 22:41:50 +0900

    docs: ✏️ add comment

--- setupFiles.js
+++ setupFiles.js
@@ -1,6 +1,9 @@
 import { decorators as nextjsDecorators } from '@storybook/nextjs/preview.js';
 import { setProjectAnnotations } from '@storybook/react';
 import * as globalStorybookConfig from './.storybook/preview';
+// also not work with ↓
+// import * as globalStorybookConfig from './.storybook/preview.js';
+
 
 const decorators = [...nextjsDecorators, ...globalStorybookConfig.decorators];
 
